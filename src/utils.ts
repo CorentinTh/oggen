5a7ebfbb09ac0a1eaade9f4af859c03bb667079f
Corentin Thomasset <corentin.thomasset74@gmail.com>
2022-08-18 18:20:10 +0200

    refactor(clean): cleaned nullish management

--- src/utils.ts
+++ src/utils.ts
@@ -2,4 +2,8 @@ export { isObject, toSnakeCase };
 
 const isObject = (v: unknown) => typeof v === 'object' && !Array.isArray(v) && v !== null;
 
-const toSnakeCase = (s: string) => (s.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g) ?? []).map((x: string) => x.toLowerCase()).join('_');
+const toSnakeCase = (s: string) =>
+  s
+    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+    ?.map((x: string) => x.toLowerCase())
+    .join('_') ?? '';
